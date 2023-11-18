44dbaf2b88ff6712e9f41cd208d94e2c6134679c
sadohu <sadohu26@yahoo.com>
2023-11-17 20:47:57 -0500

    [Add] DataXML Implementation

---
+++ Sesion04_DataXML/src/DataXML/ClassClientType.java
@@ -0,0 +1,42 @@
+package DataXML;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+
+public class ClassClientType {
+	private int idtipocliente;
+	private String nombre;
+	//declaramos una variable de tipo cliente
+	private List<ClassClient> clientes;
+	
+	
+	public ClassClientType(){
+		
+	}   //fin del constructor...
+	
+	public int getIdtipocliente() {
+		return idtipocliente;
+	}
+	
+	@XmlElement(name="cliente")
+	public void setIdtipocliente(int idtipocliente) {
+		this.idtipocliente = idtipocliente;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public List<ClassClient> getCliente() {
+		return clientes;
+	}
+	
+	public void setCliente(List<ClassClient> clientes) {
+		this.clientes = clientes;
+	}
+}

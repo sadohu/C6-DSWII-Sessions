6a43e76af40e6f6b0c8aca309dd270d7b2ac75f0
sadohu <sadohu26@yahoo.com>
2023-11-17 21:31:45 -0500

    [Add] Xml Reader

---
+++ Sesion05_DataXML/ProyectoMaven-DataXML/src/main/java/DataXML/ClassReadXML.java
@@ -0,0 +1,35 @@
+package DataXML;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+public class ClassReadXML {
+	
+	// Método para leer archivos xml
+	public void readXml(){
+		try {
+			// Cerar un reader mediante el contexto JAXB
+			JAXBContext reader = JAXBContext.newInstance(ClassClientType.class);
+			Unmarshaller unmarshaller = reader.createUnmarshaller();
+			
+			// Buscamos el archivo con File
+			File xmlFile = new File("E:/archivoxml/cliente.xml");
+			
+			// Parsear el archivo Xml al objeto ClientType
+			ClassClientType clientType =(ClassClientType)unmarshaller.unmarshal(xmlFile);
+			
+			// Bucle para visualizar la información
+			for (ClassClient item : clientType.getCliente()) {
+				System.out.println("Codigo: " + item.getIdcliente());
+				System.out.println("Nombres: " + item.getNombre() + " " + item.getApellido());
+				System.out.println("Telefono: " + item.getTelefono());
+			}
+			
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+	}
+}

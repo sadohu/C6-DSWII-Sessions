f26199d3e0c3a983dda337f241886ea16b0bf89f
sadohu <sadohu26@yahoo.com>
2023-12-13 22:21:43 -0500

    [Add] RegistrarAutor.jsp and method post in Controller

--- Sesion09_WebServicesClient/src/main/java/Controller/AutorController.java
+++ Sesion09_WebServicesClient/src/main/java/Controller/AutorController.java
@@ -31,6 +31,23 @@ public class AutorController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		// response.getWriter().append("Served at: ").append(request.getContextPath());
+		String action = request.getParameter("action");
+		if(action != null){
+			switch(action){
+			case "create":
+				request.getRequestDispatcher("/RegistrarAutor.jsp").forward(request, response);
+				break;
+			case "read":
+				break;
+			case "update":
+				break;
+			case "delete":
+				break;
+			case "search":
+				break;
+			}
+		}
+		
 		AutorService autorService = new AutorService();
 		List<Autor> autors = autorService.autores();
 		request.setAttribute("list", autors);
@@ -43,6 +60,20 @@ public class AutorController extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		// doGet(request, response);
+		
+		String nomautor = request.getParameter("nomautor");
+		String descrip = request.getParameter("descrip");
+		
+		Autor autor = new Autor();
+		autor.setNomautor(nomautor);
+		autor.setDescrip(descrip);
+		
+		AutorService autorService = new AutorService();
+		autorService.saveAutor(autor);
+		
+		List<Autor> autors = autorService.autores();
+		request.setAttribute("list", autors);
+		
 		request.getRequestDispatcher("/ListadoAutores.jsp").forward(request, response);
 	}
 
